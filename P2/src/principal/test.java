99ee15362e2ec012b633b7cea6efa19756d6a76e
drako9 <jorge.alemangonzalez@gmail.com>
2015-10-27 16:20:52 +0100

    Primer comit

---
+++ P2/src/principal/test.java
@@ -0,0 +1,22 @@
+package principal;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author drako9
+ */
+public class test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
